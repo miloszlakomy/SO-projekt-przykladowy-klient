334ac0d16694d42fbe797b704945ad4d2e1630fe
miloszlakomy <miloszzz@gmail.com>
2013-06-14 16:33:57 +0200

    Merged deadline2013-ognisko into subdirectory /foo/bar

---
+++ foo/bar/simple/display.go
@@ -0,0 +1,138 @@
+package simple
+
+import "fmt"
+import "runtime"
+import "github.com/robryk/deadline2013-ognisko/comm"
+import "net/http"
+import "html/template"
+import "bufio"
+
+type MenView Simple
+
+var menViewTempl = template.Must(template.New("menview").Parse(`
+<table><tr><td>ID</td><td>Pos</td><td>Last island</td><td>Destination</td><td>Role</td><td>BusyFor</td><td>StickCount</td></tr>
+{{range $id, $val := .}}
+<tr id="{{$id}}"><td>{{$id}}</td><td>{{if $val.Info}}{{$val.Info.Pos}}{{else}}Dead{{end}}</td><td>{{$val.Status.LastIsland}}</td><td>{{$val.Status.CurrentDestination}}</td>
+{{if $val.Info}}<td>{{$val.Info.Role}}</td><td>{{$val.Info.BusyFor}}</td><td>{{$val.Info.StickCount}}</td>{{else}}<td/><td/><td/>{{end}}</tr>
+{{end}}
+</table>`))
+
+func (mv *MenView) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	simp := (*Simple)(mv)
+	simp.mu.Lock()
+	defer simp.mu.Unlock()
+
+	type man struct {
+		Status ManStatus
+		Info *comm.ManInfo
+	}
+
+	men := map[int]man{}
+	for id, status := range simp.Men {
+		var pmi *comm.ManInfo
+		if mi, ok := simp.Game.Men[id]; ok {
+			pmi = &mi
+		}
+		men[id] = man{Status: *status, Info: pmi}
+	}
+	menViewTempl.Execute(rw, men)
+}
+
+type MapView Simple
+
+func (mv *MapView) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	simp := (*Simple)(mv)
+    simp.mu.Lock()
+    defer simp.mu.Unlock()
+	wr := bufio.NewWriter(rw)
+
+	wr.WriteString(`<html><head><title>Map</title></head><body><table>`)
+	men := map[comm.Pos]int{}
+	for id, m := range simp.Game.Men {
+		men[m.Pos] = id
+	}
+	for x := 1; x <= simp.Game.Wd.EdgeLength; x++ {
+		// not sure if needed:
+		simp.mu.Unlock()
+		runtime.Gosched()
+		simp.mu.Lock()
+		wr.WriteString(`<tr>`)
+		for y := 1; y <= simp.Game.Wd.EdgeLength; y++ {
+			water, known := simp.Game.Water[comm.Pos{x, y}]
+			var s string
+			if !known {
+				s = "?"
+			} else if water {
+				s = "_"
+			} else {
+				ii := simp.Game.Islands[comm.Pos{x, y}]
+				v := ii.Sticks - ii.MySticks
+				v = (v + 9) / 10
+				if v < 10 {
+					s = string(rune('0' + v))
+				} else if v < 36 {
+					s = string(rune('A' + v - 10))
+				} else {
+					s = "*"
+				}
+			}
+			if id, ok := men[comm.Pos{x, y}]; ok {
+				s = fmt.Sprintf(`<a href="/men#%d">%s</a>`, id, s)
+			}
+			fmt.Fprintf(wr, `<td>%s</td>`, s)
+		}
+		wr.WriteString(`</tr>`)
+	}
+	wr.WriteString(`</table></body></html>`)
+	wr.Flush()
+}
+
+type OverviewView Simple
+
+var overviewViewTempl = template.Must(template.New("menview").Parse(`
+<table>
+<tr><td>Edge length</td><td>{{.Wd.EdgeLength}}</td></tr>
+<tr><td>Islands</td><td>{{.Wd.IslandCount}}</td></tr>
+<tr><td>Bonfire Limit</td><td>{{.Wd.BonfireLimit}}</td></tr>
+<tr><td>Bonfire Coefficient</td><td>{{.Wd.BonfireCoeff}}</td></tr>
+<tr><td>Move length [s]</td><td>{{.Wd.MoveTime}}</td></tr>
+<tr><td>Result coefficient</td><td>{{.Wd.ResultCoeff}}</td></tr>
+<tr><td>Turns left</td><td>{{.Wd.TurnsLeft}}</td></tr>
+<tr><td>Fire?</td><td>{{.Wd.Fire}}</td></tr>
+<tr><td>Stick points</td><td>{{.Wd.StickPoints}}</td></tr>
+<tr><td>Marked sticks</td><td>{{.Wd.MarkedSticks}}</td></tr>
+<tr><td>Held sticks</td><td>{{.Wd.HeldSticks}}</td></tr>
+</table><p>Biggest locations:<ol>
+{{range $val := .Wd.BiggestLocations}}
+<li>{{$val.Sticks}} at ({{$val.Pos.X}}, {{$val.Pos.Y}})</li>
+{{end}}
+</ol>`))
+
+func (ov *OverviewView) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	simp := (*Simple)(ov)
+	simp.mu.Lock()
+	defer simp.mu.Unlock()
+
+	type man struct {
+		Status ManStatus
+		Info *comm.ManInfo
+	}
+
+	men := map[int]man{}
+	for id, status := range simp.Men {
+		var pmi *comm.ManInfo
+		if mi, ok := simp.Game.Men[id]; ok {
+			pmi = &mi
+		}
+		men[id] = man{Status: *status, Info: pmi}
+	}
+	overviewViewTempl.Execute(rw, simp.Game)
+	menViewTempl.Execute(rw, men)
+
+}
+
+
+
+
+
+
---
+++ foo/bar/simple/simple.go
@@ -0,0 +1,285 @@
+package simple
+
+import "github.com/robryk/deadline2013-ognisko/comm"
+import "fmt"
+import "log"
+import "sync"
+import "flag"
+import "io/ioutil"
+import "path/filepath"
+import "encoding/json"
+import "os"
+
+var StatePath = flag.String("state", "", "")
+
+var _ = fmt.Printf
+
+type ManStatus struct {
+	LastIsland         comm.Pos
+	CurrentDestination comm.Pos
+}
+
+type MenStatusArr map[int]*ManStatus
+
+func (w MenStatusArr) MarshalJSON() ([]byte, error) {
+	m := make(map[string]*ManStatus, len(w))
+	for k, v := range w {
+		kk, err := json.Marshal(k)
+		if err != nil {
+			return nil, err
+		}
+		m[string(kk)] = v
+	}
+	r, err := json.Marshal(m)
+	return r, err
+}
+
+func (w *MenStatusArr) UnmarshalJSON(buf []byte) error {
+	var m map[string]*ManStatus
+	if err := json.Unmarshal(buf, &m); err != nil {
+		return err
+	}
+	*w = make(MenStatusArr)
+	for k, v := range m {
+		var kk int
+		if err := json.Unmarshal([]byte(k), &kk); err != nil {
+			return err
+		}
+		(*w)[kk] = v
+	}
+	return nil
+}
+
+
+
+
+type Simple struct {
+	mu   sync.Mutex `json:"-"`
+	Game *comm.Game
+	Men  MenStatusArr
+}
+
+func NewSimple() *Simple {
+	c, err := comm.NewConn()
+	if err != nil {
+		panic(err)
+	}
+
+	s := &comm.Server{Conn: c}
+	simp := &Simple{
+		Game: &comm.Game{Srv: s},
+		Men:  make(map[int]*ManStatus),
+	}
+	if *StatePath == "" {
+		return simp
+	}
+	file, err := os.Open(*StatePath)
+	if err != nil {
+		log.Printf("Can't open state file: %s", err.Error())
+		return simp
+	}
+	defer file.Close()
+	dec := json.NewDecoder(file)
+	err = dec.Decode(&simp)
+	if err != nil {
+		log.Printf("Error unmarshalling: %s", err.Error())
+		//fixme: delete and abort?
+	}
+	return simp
+}
+
+func (simp* Simple) saveState() {
+	if *StatePath == "" {
+		return
+	}
+	file, err := ioutil.TempFile(filepath.Dir(*StatePath), filepath.Base(*StatePath))
+	if err != nil {
+		log.Printf("Cannot create a temp file: %s", err.Error())
+		return
+	}
+	defer os.Remove(file.Name())
+
+	enc := json.NewEncoder(file)
+	err = enc.Encode(*simp)
+	if err == nil {
+		err = file.Close()
+	}
+	if err != nil {
+		log.Printf("Error serializing state: %s", err.Error())
+		return
+	}
+
+	if err := os.Rename(file.Name(), *StatePath); err != nil {
+		log.Printf("Error overwriting old serialized file: %s", err.Error())
+	}
+}
+
+func (simp *Simple) Loop() {
+	for {
+		simp.mu.Lock()
+		simp.oneStep()
+		simp.saveState()
+		simp.mu.Unlock()
+		simp.Game.Srv.Wait()
+	}
+}
+
+func (simp *Simple) myBonfire(id int) comm.Location {
+	mi := simp.Game.Men[id]
+	r := simp.Game.Wd.BiggestLocations[0]
+	countCloser := 0
+	for _, mmi := range simp.Game.Men {
+		if mmi.Pos.Distance(r.Pos) <= mi.Pos.Distance(r.Pos) {
+			countCloser++
+		}
+	}
+	if countCloser < 2 {
+		return r
+	}
+	for _, v := range simp.Game.Wd.BiggestLocations {
+		if v.Sticks >= simp.Game.Wd.BonfireLimit / 2 && v.Pos.Distance(mi.Pos) <= r.Pos.Distance(mi.Pos) {
+			r = v
+		}
+	}
+	return r
+}
+
+func (simp *Simple) bonfireDirection(id int) comm.Pos {
+	return simp.Game.Men[id].Pos.Direction(simp.myBonfire(id).Pos)
+}
+
+func (simp *Simple) hasGuard() bool {
+	for _, mi := range simp.Game.Men {
+		if mi.Role == comm.RoleGuard {
+			return true
+		}
+	}
+	return false
+}
+
+func (simp *Simple) findNext(id int) comm.Pos {
+	var dest comm.Pos
+	mi := simp.Game.Men[id]
+	status := simp.Men[id] // exists, because oneStep()
+	//bonfire := simp.Game.Wd.BiggestLocations[0]
+	bonfire := simp.myBonfire(id)
+	if float64(bonfire.Sticks + bonfire.Pos.Distance(mi.Pos)) > 0.5*float64(simp.Game.Wd.BonfireLimit) && status.LastIsland != bonfire.Pos && mi.Pos != bonfire.Pos {
+		return bonfire.Pos
+	}
+	//if float64(bonfire.Sticks) > 0.1*float64(simp.Game.Wd.BonfireLimit) && status.LastIsland != bonfire.Pos && bonfire.Pos.Distance(mi.Pos) < 20 {
+	//	return bonfire.Pos
+	//}
+	mult := float64(0.8)
+	//if bonfire.Sticks < simp.Game.Wd.BonfireLimit / 2 {
+	//	mult = 0
+	//}
+	minDist := float64((mult + 1) * 1e12)
+	for _, fi := range simp.Game.Islands {
+		if fi.Pos == mi.Pos {
+			continue
+		}
+		// if i have nothing, i need to go somewhere i can get something
+		if (fi.Sticks == 0 || simp.Game.Guarded[fi.Pos]) && mi.StickCount == 0 {
+			continue
+		}
+		if mi.Role != comm.RoleCaptain && fi.Sticks + mi.StickCount >= 100 && !simp.Game.Guarded[fi.Pos] && mi.Pos.Distance(fi.Pos) < 30 {
+			return fi.Pos
+		}
+		if (fi.Sticks == fi.MySticks || simp.Game.Guarded[fi.Pos]) && bonfire.Sticks < simp.Game.Wd.BonfireLimit / 3 {
+			continue // not always!
+		}
+		if status.LastIsland == fi.Pos {
+			continue
+		}
+		penalty := mult * float64(fi.Pos.Distance(bonfire.Pos) - mi.Pos.Distance(bonfire.Pos))
+		dist := float64(mi.Pos.Distance(fi.Pos)) + penalty
+		if dist < minDist {
+			minDist = dist
+			dest = fi.Pos
+		}
+	}
+	return dest
+}
+
+func (simp *Simple) oneStep() {
+	if err := simp.Game.Init(); err != nil {
+		if err == comm.ErrNewGame {
+			if *StatePath != "" {
+				os.Remove(*StatePath)
+			}
+			os.Exit(0)
+		}
+		panic(err)
+	}
+	for id, mi := range simp.Game.Men {
+		status := simp.Men[id]
+		if status == nil {
+			status = new(ManStatus)
+			simp.Men[id] = status
+		}
+		if mi.Role == comm.RoleGuard && mi.Pos != simp.Game.Wd.BiggestLocations[0].Pos { // guarding will be funny with multiple bonfires
+			simp.Game.Srv.StopGuard(id)
+		}
+		field := simp.Game.Islands[mi.Pos]
+		if field != nil {
+			status.CurrentDestination = comm.Pos{}
+		}
+		if field != nil && mi.StickCount > 0 {
+			if mi.Pos != status.LastIsland {
+				simp.Game.Srv.Drop(id)
+				status.LastIsland = comm.Pos{}
+				field.Sticks += mi.StickCount // hack
+				mi.StickCount = 0 // hack: we've just dropped and don't know that yet
+			}
+		}
+		if field != nil && field.Sticks >= simp.Game.Wd.BonfireLimit {
+			simp.Game.Srv.Ignite(id)
+			continue
+		}
+		if field != nil && field.Sticks >= 100 && !simp.Game.Guarded[mi.Pos] && mi.Role != comm.RoleCaptain {
+			simp.Game.Srv.Build(id)
+			continue
+		}
+		if mi.Pos == simp.Game.Wd.BiggestLocations[0].Pos && !simp.hasGuard() && mi.Role != comm.RoleCaptain && simp.Game.Wd.BiggestLocations[0].Sticks > 200 {
+			simp.Game.Srv.Guard(id)
+			continue
+		}
+		if field != nil && field.Sticks > field.MySticks && mi.StickCount < mi.Cap() && !simp.Game.Guarded[mi.Pos] {
+			if float64(field.Sticks) > 0.8 * float64(simp.Game.Wd.BonfireLimit) && float64(field.MySticks) > 0.4 * float64(field.Sticks) {
+				// don't take anything away
+			} else {
+				simp.Game.Srv.Take(id)
+			}
+			// set lastisland anyway to advance
+			status.LastIsland = mi.Pos
+			continue
+		}
+/*		var dest *comm.IslandInfo
+		for _, fi := range simp.Game.Islands {
+			if fi.Pos == mi.Pos {
+				continue
+			}
+			if fi.Sticks == fi.MySticks {
+				continue
+			}
+			if status.LastIsland == fi.Pos {
+				continue
+			}
+			if fi.Pos.Distance(mi.Pos) < minDist {
+				dir = mi.Pos.Direction(fi.Pos)
+				minDist = mi.Pos.Distance(fi.Pos)
+				dest = fi
+			}
+		}*/
+		if status.CurrentDestination == (comm.Pos{}) {
+			status.CurrentDestination = simp.findNext(id)
+		}
+		if status.CurrentDestination != (comm.Pos{0, 0}) {
+			dir := mi.Pos.Direction(status.CurrentDestination)
+			log.Printf("Man %d going from %v to %v, in direction %v.\n", id, mi.Pos, status.CurrentDestination, dir)
+			simp.Game.Srv.Move(id, dir.X, dir.Y)
+		} else {
+			log.Printf("Man %d doesn't have anywhere to go.\n", id)
+		}
+	}
+}
